81bbdbbe02f1c72fca3c163d4b3d8c0c88a1f9d0
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-10-27 16:21:59 +0000

    Fix in wrong specs

--- PokerHandsSpec.py
+++ PokerHandsSpec.py
@@ -7,9 +7,9 @@ from IllegalArgumentsException import IllegalArgumentsException
 import PokerHands
 from mockito import when
 
-straight_flush_hand = Hand("6C 7C 8C 9C T9".split())
-four_of_a_kind_hand = Hand("D9 H9 S9 C9 D7".split())
-full_house_hand = Hand("TD TC TH 7C 7D".split())
+straight_flush = Hand("6C 7C 8C 9C T9".split())
+four_of_a_kind = Hand("D9 H9 S9 C9 D7".split())
+full_house = Hand("TD TC TH 7C 7D".split())
 
 with description('Given an empty set of hands') as self:
   with context('When trying to rank the hands'):
@@ -19,43 +19,43 @@ with description('Given an empty set of hands') as self:
 with description('Given a set of one hand') as self:
   with context('When trying to rank the hands'):
     with it('should return the one and only hand'):
-      hands = [straight_flush_hand]
-      expect(poker(hands)).to(equal(straight_flush_hand))
+      hands = [straight_flush]
+      expect(poker(hands)).to(equal(straight_flush))
 
 with description('Given four of a kind versus full house') as self:
   with context('When trying to rank the hands'):
     with it('should return the full house hand'):
-      hands = [four_of_a_kind_hand, full_house_hand]
-      when(PokerHands).hand_rank(four_of_a_kind_hand).thenReturn(2)
-      when(PokerHands).hand_rank(full_house_hand).thenReturn(3)
+      hands = [four_of_a_kind, full_house]
+      when(PokerHands).hand_rank(full_house).thenReturn(1)
+      when(PokerHands).hand_rank(four_of_a_kind).thenReturn(2)
       
-      expect(poker(hands)).to(equal(full_house_hand))
+      expect(poker(hands)).to(equal(four_of_a_kind))
 
 with description('Given full house versus full house') as self:
   with context('When trying to rank the hands'):
     with it('should return the full house hand'):
-      hands = [full_house_hand, full_house_hand]
-      when(PokerHands).hand_rank(full_house_hand).thenReturn(3)
+      hands = [full_house, full_house]
+      when(PokerHands).hand_rank(full_house).thenReturn(1)
       
-      expect(poker(hands)).to(equal(full_house_hand))
+      expect(poker(hands)).to(equal(full_house))
 
 with description('Given 99 straight flushes versus full house') as self:
   with context('When trying to rank the hands'):
     with it('should return the full house hand'):
-      hands = [full_house_hand] + 99 * [straight_flush_hand]
-      when(PokerHands).hand_rank(full_house_hand).thenReturn(3)
-      when(PokerHands).hand_rank(straight_flush_hand).thenReturn(1)
+      hands = [full_house] + 99 * [straight_flush]
+      when(PokerHands).hand_rank(full_house).thenReturn(1)
+      when(PokerHands).hand_rank(straight_flush).thenReturn(3)
 
-      expect(poker(hands)).to(equal(full_house_hand))
+      expect(poker(hands)).to(equal(straight_flush))
 
 with description('Given a set of straight flush, four of a kind and full house') as self:
   with context('When trying to rank the hands'):
     with it('should return the full house hand'):
-      hands = [straight_flush_hand, four_of_a_kind_hand, full_house_hand]
-      when(PokerHands).hand_rank(straight_flush_hand).thenReturn(1)
-      when(PokerHands).hand_rank(four_of_a_kind_hand).thenReturn(2)
-      when(PokerHands).hand_rank(full_house_hand).thenReturn(3)
+      hands = [straight_flush, four_of_a_kind, full_house]
+      when(PokerHands).hand_rank(full_house).thenReturn(1)
+      when(PokerHands).hand_rank(four_of_a_kind).thenReturn(2)
+      when(PokerHands).hand_rank(straight_flush).thenReturn(3)
       
-      expect(poker(hands)).to(equal(full_house_hand))
+      expect(poker(hands)).to(equal(straight_flush))
 
 
